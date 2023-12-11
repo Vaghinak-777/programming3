ed6b79912ba06b368a761a75ebe0ea8382d0872e
Vaghinak-777 <Vaghinak.v.g@tumo.org>
2023-12-11 17:22:04 +0400

    LEsson 5

---
+++ GameOfLife/server/grass.js
@@ -0,0 +1,23 @@
+let LivingCreature = require("./livingCreature")
+
+module.exports = class Grass extends LivingCreature{
+    
+
+    
+
+    mul(){
+        this.multiply++;
+        let emptyCell = this.chooseCell(0);
+        let newCell = random(emptyCell);
+
+        if(newCell && this.multiply >= 4){
+            let newX = newCell[0];
+            let newY = newCell[1];
+            matrix[newY][newX] = 1;
+            let newGrass = new Grass(newX, newY);
+            grassArray.push(newGrass);
+            this.multiply = 0;
+        }
+    }
+
+}
---
+++ GameOfLife/server/grassEater.js
@@ -0,0 +1,101 @@
+class GrassEater extends LivingCreature{
+    constructor(x, y) {
+        super(x,y)
+        this.energy = 8;
+
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+
+    chooseCell(character) {
+        this.getNewCoordinates();
+
+        return super.chooseCell(character);
+    }
+
+    move() {
+        this.energy--
+        let emptyCells = this.chooseCell(0)
+        let newCell = random(emptyCells)
+        if (newCell) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[this.y][this.x] = 0
+            matrix[newY][newX] = 2
+            this.x = newX
+            this.y = newY
+        }
+
+        if (this.energy <= 0) {
+            this.die()
+        }
+    }
+    eat() {
+        let foods = this.chooseCell(1)
+        let food = random(foods)
+        if (food) {
+            this.energy++;
+            console.log(this.energy);
+
+            matrix[this.y][this.x] = 0
+            let newX = food[0]
+            let newY = food[1]
+            matrix[newY][newX] = 2
+            this.x = newX
+            this.y = newY
+            for (var i in grassArray) {
+                if (newX == grassArray[i].x && newY == grassArray[i].y) {
+                    grassArray.splice(i, 1);
+                    break;
+                }
+            }
+            if (this.energy >= 10) {
+                this.mul()
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+  
+    mul() {
+        let emptyCell = this.chooseCell(0)
+        let newCell = random(emptyCell)
+
+        if (newCell ) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+
+            matrix[newY][newX] = 2
+
+            let grEat = new GrassEater(newX, newY)
+
+            grassEaterArr.push(grEat)
+
+
+        }
+    }
+
+    die() {
+        matrix[this.y][this.x] = 0;
+        for (var i in grassEaterArr) {
+            if (this.x == grassEaterArr[i].x && this.y == grassEaterArr[i].y) {
+                grassEaterArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+}
